3e4da01d294abf39519050b579e3733d123b70f8
chandradewi26 <chandradewi2691@gmail.com>
2020-09-13 18:33:30 +1000

    added BusinessLogic

---
+++ UpBanking/UpBanking.BusinessLogic/DomainModel/Transaction.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UpBanking.BusinessLogic.DomainModel
+{
+    public class Transaction
+    {
+        public Transaction()
+        {
+
+        }
+
+        public Transaction(string description, decimal value)
+        {
+            if (value < 0)
+                throw new Exception("transaction cant be less than 0");
+            Description = description;
+            Value = value;
+        }
+        public string Description { get; set; }
+        public decimal Value { get; set; }
+    }
+}
---
+++ UpBanking/UpBanking.BusinessLogic/Logic/TransactionLogic.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using UpBanking.BusinessLogic.DomainModel;
+
+namespace UpBanking.BusinessLogic.Logic
+{
+    public class TransactionLogic
+    {
+        public Transaction GetNewTransaction()
+        {
+            var transaction2 = new Transaction("Butcher", 50);
+
+
+            return transaction2;
+        }
+    }
+}
